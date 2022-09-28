dd27193cffee044267a288ed2252a6ad596c536e
mahian <mahianahmad9@gmail.com>
2022-09-29 01:32:23 +0600

    add navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div>
-            <div>
-                <img src='assets/logo.png' alt="..."/>
+        <div className='bg-primary text-white p-2'>
+            <div className='d-flex align-items-center'>
+                <img style={{height: '40px', width: '40px'}} src='assets/logo.png' alt="..."/>
                 <h2>practice-zone.</h2>
             </div>
         </div>
