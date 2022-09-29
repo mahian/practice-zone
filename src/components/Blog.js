7b7b31cb671ad04004a364879236b43f0872b4bb
mahian <mahianahmad9@gmail.com>
2022-09-29 17:12:45 +0600

    add blog file

---
+++ src/components/Blog.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Blog = () => {
+    const post = [
+        {
+            quetion: 'que1',
+            ans: 'ans2',
+        },
+        {
+            quetion: 'que2',
+            ans: 'ans2',
+        },
+        {
+            quetion: 'que3',
+            ans: 'ans3',
+        }
+    ]
+    return (
+        <div>
+            Blog
+        </div>
+    );
+};
+
+export default Blog;
