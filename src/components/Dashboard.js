6fbad595f1050f1cb6a21842a61368acfd02da2e
mahian <mahianahmad9@gmail.com>
2022-09-29 13:25:11 +0600

    customize design

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -8,43 +8,35 @@ const Dashboard = (props) => {
     }
     return (
         <div className='p-4'>
-            <div className='d-flex'>
+            <div className='d-flex align-items-center'>
                 <img className='profile-img' src='https://scontent.fjsr8-1.fna.fbcdn.net/v/t39.30808-6/295180433_1389599218214479_7405679272567519554_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=3UEfOhgji9EAX_xtCjJ&_nc_ht=scontent.fjsr8-1.fna&oh=00_AT9BpmL7reqHuILI_64mUl2ROrqZdgYlJDhF4-L3KxpCkQ&oe=6339226F' alt="..."/>
-                <div>
-                    <h4>Mahian ahmad</h4>
-                    <p>location here</p>
+                <div className='ms-3'>
+                    <h4 className='m-0'>Mahian ahmad</h4>
+                    <p className='m-0'>location here</p>
                 </div>
             </div>
-            <div className='d-flex justify-content-between bg-light p-4 rounded'>
-                <div className='d-flex flex-column align-items-center'>
-                    <h3>100</h3>
-                    <span>kg</span>
-                </div>
-                <div className='d-flex flex-column align-items-center'>
-                    <h3>100</h3>
-                    <span>kg</span>
-                </div>
-                <div className='d-flex flex-column align-items-center'>
-                    <h3>100</h3>
-                    <span>kg</span>
+            <div className='bg-prime text-white p-4 mt-4 rounded'>
+                <div className='d-flex justify-content-between'>
+                    <p className='m-0 fw-bold'>Typing</p>
+                    <p className='m-0 fw-bold'>60/WPM</p>
                 </div>
             </div>
-            <h4 className='py-2'>add a break</h4>
-            <div className='d-flex justify-content-between bg-light p-4 rounded'>
+            <h4 className='py-2 mt-3'>add a break</h4>
+            <div className='d-flex flex-wrap bg-light p-2 rounded'>
                 {
-                    times.map(time => <button className='break-button' onClick={()=> handleBreakBtn(time)}>{time}</button>)
+                    times.map(time => <button className='break-button' onClick={()=> handleBreakBtn(time)}>{time} minutes</button>)
                 }
             </div>
             <h4 className='py-2'>Practice details</h4>
             <div className='d-flex justify-content-between bg-light p-4 rounded'>
-                <p>practice time</p>
-                <span>{props.time}min</span>
+                <p className='m-0 fw-bold'>practice time :</p>
+                <p className='m-0 fw-bold text-secondary'>{props.time}min</p>
             </div>
-            <div className='d-flex justify-content-between bg-light p-4 rounded mt-2'>
-                <p>brake time</p>
-                <span>{breakTime}</span>
+            <div className='d-flex justify-content-between bg-light p-4 rounded'>
+                <p className='m-0 fw-bold'>Breake time :</p>
+                <p className='m-0 fw-bold text-secondary'>{breakTime}min</p>
             </div>
-            <button className='btn btn-primary mt-3'>Activity completed</button>
+            <button className='btn btn-primary mt-4 w-100 fw-bold'>Activity completed</button>
         </div>
     );
 };
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div className='bg-primary text-white p-2'>
+        <div className='bg-prime text-white py-2 px-5'>
             <div className='d-flex align-items-center'>
                 <img style={{height: '40px', width: '40px'}} src='assets/logo.png' alt="..."/>
                 <h2>PRACTICE-ZONE.</h2>
